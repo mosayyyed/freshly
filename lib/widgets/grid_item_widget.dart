9825fb36c3768f06ef22cd41f5ba86aa258b79d7
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2024-08-20 22:31:34 +0300

    done grid_item_widget.dart

---
+++ lib/widgets/grid_item_widget.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+import '../models/recipe_model.dart';
+import '../screens/recipe_screen.dart';
+
+class RecipeGridItem extends StatelessWidget {
+  const RecipeGridItem({
+    super.key,
+    required this.recipe,
+    required this.isFavorite,
+    required this.onFavoriteToggle,
+  });
+
+  final RecipeModel recipe;
+  final bool isFavorite;
+  final VoidCallback onFavoriteToggle;
+
+  @override
+  Widget build(BuildContext context) {
+    const TextStyle titleStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 12.0,
+    );
+
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => RecipeView(recipe: recipe),
+          ),
+        );
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 150.0,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(12.0),
+                  ),
+                  image: DecorationImage(
+                    image: NetworkImage(recipe.image),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 8.0,
+                right: 8.0,
+                child: GestureDetector(
+                  onTap: onFavoriteToggle,
+                  child: Container(
+                    padding: const EdgeInsets.all(4.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.7),
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(
+                      Icons.favorite,
+                      color: isFavorite ? Colors.red : Colors.white,
+                      size: 24.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Text(
+              recipe.title,
+              style: titleStyle,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
